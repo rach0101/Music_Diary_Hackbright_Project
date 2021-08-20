0d46134270744fcd8ff5594fc5078275adf5ad47
Rachel Cleak <rachelmcleak@gmail.com>
2021-08-20 16:53:09 -0700

    finished for the week

--- static/js/diary.js
+++ static/js/diary.js
@@ -5,24 +5,31 @@
 // give form an id
 $(document).ready( () => {
     $('#music_search').on('submit', (event) => {
+        // Display results of song search on submit
         event.preventDefault();
         
         const data = {"name": $("#music_search_input").val()};
 
         $.post('/diary_api.json', data, (response) => {
-            
-            console.log((response));
-     
-            console.log(response.name);
-            
+            // Loop through JSON response data from Spotify API
+            // Append 5 songs with links and album art to the DOM
             for (const element of response){
                 console.log(element.name);
                 $('#search_results').append(
-                                `<div> 
-                                    <img src="${element.album.images[2].url}">
-                                    <a href="${element.external_urls.spotify}">${element.name} </a>
+                                `<div>
+                                    <button id="${element.uri}"> 
+                                        <img src="${element.album.images[2].url}">
+                                        <a href="${element.external_urls.spotify}">${element.name} </a>
+                                    </button>
                                 </div>`);
-            }; 
+                                
+            };    
         });
     });
 });
+ // add event listener to div element?
+$('button').on('click', (event) =>   {
+    event.preventDefault();
+    $('#new_post').append(`<div>I am a new post!</div>`);
+});
+
